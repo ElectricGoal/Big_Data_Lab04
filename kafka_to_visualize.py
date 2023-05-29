48be3ddbe7d5d6e324443c47c6f63cf815c4b4b0
HXT-US <hatruong3996@gmail.com>
2023-05-29 14:01:55 +0700

    Update kafka_to_visualize.py

--- kafka_to_visualize.py
+++ kafka_to_visualize.py
@@ -1,19 +1,76 @@
 from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-from pyspark.sql.functions import from_json, to_json, explode, struct
+from pyspark.sql.functions import *
+import re
+from textblob import TextBlob
+import matplotlib
+matplotlib.use('Agg')  # Use a non-interactive backend
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Function to process the data
-def process(batch_df, batch_id):
-    print(f"Batch ID: {batch_id}")
-    batch_df.show(truncate=False)
-    batch_df.groupBy("Tweet").count().show(truncate=False)
+spark = SparkSession.builder.getOrCreate()
 
+def analyze_sentiment(tweet):
+    blob = TextBlob(tweet)
+    return blob.sentiment.polarity
+
+analyze_sentiment_udf = udf(analyze_sentiment, StringType())
+
+# Remove unwanted components from the "Tweet" column using regex
+def clean_tweet(tweet):
+    cleaned_text = re.sub(r"@\w+|#[^\s]+|http\S+|[^a-zA-Z0-9\s]", "", tweet)
+    return cleaned_text
+
+def sentiment(res_df):
+    clean_tweet_udf = udf(clean_tweet, StringType())
+    res_df = res_df.withColumn("CleanedTweet", clean_tweet_udf("Tweet"))
+
+    res_df = res_df.withColumn("Sentiment", analyze_sentiment_udf("CleanedTweet"))
+
+    # Define thresholds for negative, positive, and neutral sentiments
+    positive_threshold = 0.2
+    negative_threshold = -0.2
+
+    visualize_df = res_df
+    res_df = res_df.withColumn("Sentiment", when(res_df["Sentiment"] > positive_threshold, "positive").when(res_df["Sentiment"] < negative_threshold, "negative").otherwise("neutral"))
+
+    return res_df, visualize_df
+
+# Function to visualize and save the plot for each batch of data
+def visualize_batch(batch_df, batch_id):
+    _, visualize_df = sentiment(batch_df)
+
+    # Aggregate the sentiment analysis results over a specific time window
+    windowed_df = visualize_df.groupBy(window("Date", "1 minute")).agg(avg("Sentiment").alias("AverageSentiment"))
+
+    # Collect the aggregated data into the driver
+    results = windowed_df.collect()
+
+    # Extract the window start time and average sentiment values
+    window_starts = [str(row['window']['start']) for row in results]
+    sentiment_values = [row['AverageSentiment'] for row in results]
+
+    # Create the plot
+    plt.figure(figsize=(12, 6))
+    sns.lineplot(x=window_starts, y=sentiment_values)
+    plt.xlabel("Time Window")
+    plt.ylabel("Average Sentiment")
+    plt.title("Sentiment Analysis over Time")
+    plt.xticks(rotation=45)
+
+    # Save the plot as an image file
+    filename = f"sentiment_analysis_{batch_id}.png"
+    plt.savefig(filename)
+    plt.close()
+
+# Initialize the SparkSession
 spark = SparkSession.builder.appName("kafka_consumer").getOrCreate()
 
 kafka_bootstrap_servers = "localhost:9092"
 kafka_topic = "twitterstream"
 
+# Read the streaming data from Kafka
 kafka_stream = spark \
     .readStream \
     .format("kafka") \
@@ -22,8 +79,10 @@ kafka_stream = spark \
     .option("startingOffsets", "earliest") \
     .load()
 
+# Extract the value from Kafka messages
 kafka_stream_value = kafka_stream.selectExpr("CAST(value AS STRING)")
 
+# Define the schema for parsing the JSON data
 schema = StructType([
     StructField("_id", StringType(), True),
     StructField("Date", StringType(), True),
@@ -40,13 +99,12 @@ schema = StructType([
     StructField("UserDescription", StringType(), True)
 ])
 
-
+# Parse the streaming data and select the required columns
 parsed_df = kafka_stream_value.withColumn("value", from_json("value", schema))
 res_df = parsed_df.select("value.*")
 
-# Perform further processing on the streaming data
-query = res_df.writeStream.trigger(processingTime='20 seconds').foreachBatch(process).start()
+# Start the streaming query and process each batch
+query = res_df.writeStream.trigger(processingTime='20 seconds').foreachBatch(visualize_batch).start()
 
-# Start the streaming context
-# kafka_stream_value.start()
+# Wait for the streaming query to finish
 query.awaitTermination()
